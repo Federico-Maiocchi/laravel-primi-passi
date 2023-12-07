ad6c70f9646a63ccc8338491f706ae99db987bc9
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-12-07 15:47:18 +0100

    ADD: dinamyc home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+        'title' => 'Home',
+        'subtitle' => 'Description'
+
+    ];
+
+    return view('home', $data);
 });
